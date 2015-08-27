3a2db87ef7df893356f91e3d9ff47dc2e0bc7b8e
lvsheng <lvsheng@baidu.com>
2015-08-27 10:04:41 +0800

    WIP 尝试自己写插件编译ts

--- build/fis-conf.js
+++ build/fis-conf.js
@@ -13,6 +13,17 @@ fis.match('**.less', {
     rExt: '.css'
 });
 
+//TODO: 自己写一个小的插件来搞这件事？（用shelljs来exec本地的tsc进行编译）
+//build.sh中编译导致生成的js与map文件都存在于源码位置，影响源码阅读~ 不爽
+//可以使用fis插件来编译ts，但速度有点慢、而且tsc版本低，源码编译时有报错（貌似不支持export from）
+fis.match('**.ts', {
+    parser: fis.plugin('local-tsc', {
+        hello: 'conf'
+    }),
+    rExt: '.js'
+});
+
+
 fis.match('**', {
     release: '/o2o/vm/$0',
     url: '/~lvsheng/o2o/vm/$0'
